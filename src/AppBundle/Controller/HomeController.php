ea6cc862dd75486aa2df2dba0d595443cb883f14
Bhalexx <minia-ture@hotmail.com>
2018-03-13 16:44:25 +0100

    Mobile list and single mobile pages

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -10,22 +10,62 @@ use GuzzleHttp\Client;
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route(
+     *     "/{page}", 
+     *     name = "mobiles",
+     *     requirements = { "page" = "\d+" },
+     *     defaults = { "page" = 1 }
+     * )
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $page)
     {
         $client = $this->get('csa_guzzle.client.bilemo_api');
-        $uri = 'api/manufacturers';
+        $manufacturersUri = 'api/manufacturers';
+        $mobilesUri = sprintf('api/mobiles?limit=%d&offset=%s', $this->container->getParameter('mobile_limit'), $page);
         
-        $manufacturers = $client->get($uri, [
+        $headers = [
             'headers' => [
                 'Content-type' => 'application/json',
                 'Authorization' => 'Bearer '.$this->get('session')->get('access_token')
             ]
-        ]);
+        ];
+
+        $manufacturers = $client->get($manufacturersUri, $headers);
+        $mobiles = $client->get($mobilesUri, $headers);
 
         return $this->render('home/index.html.twig', [
-            // 'manufacturers' => json_decode($manufacturers->getBody(), true)
+            'manufacturers' => json_decode($manufacturers->getBody(), true),
+            'mobiles' => json_decode($mobiles->getBody(), true)
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/search/{manufacturer}/{page}",
+     *     name="search",
+     *     requirements = { "manufacturer" = "\d+", "page" = "\d+" },
+     *     defaults = { "page" = 1 }
+     * )
+     */
+    public function search(Request $request, $manufacturer, $page)
+    {
+        $client = $this->get('csa_guzzle.client.bilemo_api');
+        $manufacturersUri = 'api/manufacturers';
+        $mobilesUri = sprintf('api/mobiles?manufacturer=%d&limit=%d&offset=%s', $manufacturer, $this->container->getParameter('mobile_limit'), $page);
+        
+        $headers = [
+            'headers' => [
+                'Content-type' => 'application/json',
+                'Authorization' => 'Bearer '.$this->get('session')->get('access_token')
+            ]
+        ];
+
+        $manufacturers = $client->get($manufacturersUri, $headers);
+        $mobiles = $client->get($mobilesUri, $headers);
+
+        return $this->render('home/index.html.twig', [
+            'manufacturers' => json_decode($manufacturers->getBody(), true),
+            'mobiles' => json_decode($mobiles->getBody(), true)
         ]);
     }
 }
