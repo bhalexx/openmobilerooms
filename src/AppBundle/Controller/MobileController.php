ecead8378dccafaa5d08b1c43bc642c3e4152723
Bhalexx <minia-ture@hotmail.com>
2018-03-21 17:23:08 +0100

    API calls refactoring

--- src/AppBundle/Controller/MobileController.php
+++ src/AppBundle/Controller/MobileController.php
@@ -3,31 +3,21 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use GuzzleHttp\Client;
 
-class MobileController extends Controller
+class MobileController extends BaseController
 {
 	/**
      * @Route("/mobile/{id}", name="single_mobile", requirements={"id"="\d+"})
      */
     public function indexAction(Request $request, $id)
     {
-        $client = $this->get('csa_guzzle.client.bilemo_api');
         $uri = 'api/mobiles/'.$id;
-        
-        $headers = [
-            'headers' => [
-                'Content-type' => 'application/json',
-                'Authorization' => 'Bearer '.$this->get('session')->get('access_token')
-            ]
-        ];
 
-        $mobile = $client->get($uri, $headers);
+        $mobile = $this->request($uri);
 
         return $this->render('mobile/single.html.twig', [
-            'mobile' => json_decode($mobile->getBody(), true)
+            'mobile' => $mobile
         ]);
     }
 }
